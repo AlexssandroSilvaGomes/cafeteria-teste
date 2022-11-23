37b4aaf89c35965e5ba6bbdc1c580aea767fbf44
unknown <alexssandrosilva244@gmail.com>
2022-11-22 22:14:07 -0300

    header concluído

---
+++ eclipse-workspace/primeiroPrograma/src/primeiroPrograma/teste1.java
@@ -0,0 +1,13 @@
+package primeiroPrograma;
+
+public class teste1 {
+	
+	public static void main(String[] args) {
+		
+		int peso = 45;
+		double altura = 1.60;
+		double imc = peso / (altura * altura);
+		
+		System.out.println("Seu imc e: " + imc);
+	}
+}
